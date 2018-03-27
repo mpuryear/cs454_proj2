5738daf60bd6e0ae8c30f9077fba924b47e662a9
Mathew Puryear <mpuryear@blue.cs.sonoma.edu>
2018-03-27 12:17:26 -0700

    Added function prototypes and problem handlers

---
+++ main.py
@@ -0,0 +1,34 @@
+
+def problem1():
+    # Put problem1 code here
+    return
+
+def problem2():
+    # Put problem2 code here
+    return
+
+def problem3():
+    # Put problem3 code here
+    return
+
+
+
+def handle_prob1():
+    return 
+
+def handle_prob2():
+    return
+
+def handle_prob3():
+    return
+
+
+def main():
+    handle_prob1()
+    handle_prob2()
+    handle_prob3()
+
+
+if __name__ == "__main__":
+    main()
+    
