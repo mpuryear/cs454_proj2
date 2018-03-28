b0605c5b1790245c1be8d4a130d6e3bc5bebfc96
Mathew Puryear <mpuryear@blue.cs.sonoma.edu>
2018-03-27 18:56:42 -0700

    Completed problem2

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ def problem1():
     # Put problem1 code here
     return
 
-def problem2():
+def problem2(filename):
     # Put problem2 code here
+    # teestoost thiis fiile
+    import subprocess
+    cmd = 'egrep'
+
+    # String used to complete problem 2
+    egrep_string = '.*(.)\\1.*(.)\\2.*'
+
+    
+    cmd_list = [cmd, egrep_string, filename]
+    subprocess.run(cmd_list)
     return
 
 def problem3():
@@ -14,12 +24,16 @@ def problem3():
 
 
 def handle_prob1():
+    problem1()
     return 
 
 def handle_prob2():
+    filename = input("File to egrep(note - whitespace is considered in the language): ")
+    problem2(filename)
     return
 
 def handle_prob3():
+    problem3()
     return
 
 
