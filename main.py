fc99f3b95a23f4cec12d477965ba342400741836
Mathew Puryear <mpuryear@blue.cs.sonoma.edu>
2018-04-02 18:56:11 -0700

    Fixed input into main

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-
-def problem1():
+import Problem1
+def problem1(input1, input2):
     # Put problem1 code here
+    
+    alphabet = "0 1 2 3 4 5 6 7 8 9".split()
+    d = Problem1.gen_dfa(input2, alphabet)
+    print("True means the number is not strongly divisible:",
+          d.run_with_input_list(input1))
+    
     return
 
 def problem2():
@@ -14,6 +20,11 @@ def problem3():
 
 
 def handle_prob1():
+    u_input1 = list(str(input("Enter a number A to determine if A is strongly\
+    divisible by B: ")))
+    u_input2 = int(input("Enter a number B to determine if A is strongly divisible by B: "))
+
+    problem1(u_input1, u_input2)
     return 
 
 def handle_prob2():
